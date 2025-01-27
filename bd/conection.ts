f12ccce58b1ccf76f3f6b4b439d1fedb54cfaebb
usamahas17 <ponsiopilato1999@gmail.com>
2025-01-27 17:24:28 -0300

    treceavo cambio

--- bd/conection.ts
+++ bd/conection.ts
@@ -1,4 +1,6 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
 const db = new Sequelize(process.env.DB_NAME || "registro_2025", process.env.DB_USER || "root", process.env.DB_PASSWORD || "root", {
     host: process.env.DB_HOST || "localhost", 
     dialect: 'mysql',
