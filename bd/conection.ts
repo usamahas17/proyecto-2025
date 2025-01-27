7d55f4c50363cbb177a56b7f3f6e5fc865f2fcd3
usamahas17 <ponsiopilato1999@gmail.com>
2025-01-27 18:25:13 -0300

    septimoavo cambio

--- bd/conection.ts
+++ bd/conection.ts
@@ -3,14 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 const db = new Sequelize(process.env.DB_NAME || "registro_2025", process.env.DB_USER || "root", process.env.DB_PASSWORD || "root", {
     host: process.env.DB_HOST || "localhost", 
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejecUnauthorized: false
-        }
-    },
-    port: 5432, 
+    dialect: 'mysql',
+    port: parseInt(process.env.DB_PORT || '8889'), 
 
 });
 
