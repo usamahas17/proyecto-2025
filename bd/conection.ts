75b65a33fc9f85c313ce3d421f6147d4a3aa6264
usamahas17 <ponsiopilato1999@gmail.com>
2025-01-27 17:19:24 -0300

    doceavo cambio

--- bd/conection.ts
+++ bd/conection.ts
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
-const db = new Sequelize('registro_2025','root','root',{
-    host: 'localhost',
+const db = new Sequelize(process.env.DB_NAME || "registro_2025", process.env.DB_USER || "root", process.env.DB_PASSWORD || "root", {
+    host: process.env.DB_HOST || "localhost", 
     dialect: 'mysql',
-    port: 8889,
+    port: 8889, 
 
-} );
+});
 
 export default db;
--- index.ts
+++ index.ts
@@ -1,49 +1,55 @@
-import express, {Application} from "express";
+import express, { Application } from "express";
 import cors from "cors";
 import db from "./bd/conection";
 import bodyparse from "body-parser";
 import rutasUsuario from './rutas/usuario.ruta';
 
 
-class Server{
-    private app:Application;
-    private port:string;
+class Server {
+    private app: Application;
+    private port: string;
+    private host: string;
     private apiPaths = {
         usuarios: '/api/usuarios/'
     }
 
 
-    constructor(){
+    constructor() {
         this.app = express();
         this.port = process.env.PORT || "9000";
+        this.host = ('RENDER' in process.env) ? '0.0.0.0' : 'localhost';
         this.middlewares();
         this.dbConnection();
         this.routes();
-        
+
     }
-    middlewares(){
+    middlewares() {
         this.app.use(cors());
         this.app.use(express.json());
         this.app.use(express.static('public'));
-        this.app.use(bodyparse.urlencoded({extended:true}));
+        this.app.use(bodyparse.urlencoded({ extended: true }));
     }
-    routes(){
-        this.app.use(this.apiPaths.usuarios,rutasUsuario);
+    routes() {
+        this.app.use(this.apiPaths.usuarios, rutasUsuario);
     }
-    
-    listen(){
+
+    listen() {
+        this.app.listen(this.host,() =>{
+            console.log("el host corre en el puerto " + this.host);
+        })
         this.app.listen(this.port, () => {
             console.log("el servidor corre en el puero " + this.port);
         })
+        
     }
-    async dbConnection(){
-        try{
+    async dbConnection() {
+        try {
             await db.authenticate();
             console.log("la base de dato esta en linea");
         }
-        catch (error:any){
+        catch (error: any) {
             throw new Error(error);
-         }
+        }
     }
 
 }
