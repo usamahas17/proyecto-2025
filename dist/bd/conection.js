de42a1d67a4ba99e7707c73f42ca9a43c41fa277
usamahas17 <ponsiopilato1999@gmail.com>
2025-01-27 22:49:44 -0300

    24 cambio

--- dist/bd/conection.js
+++ dist/bd/conection.js
@@ -7,7 +7,7 @@ const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const db = new sequelize_1.Sequelize(process.env.DATABASE || "apirest_7i13", process.env.DB_USER || "apirest", process.env.DB_PASSWORD || "oml8ebqCPnAodgLCTgTc5zICw5VvaJd8", {
-    host: process.env.DB_HOST || "dpg-cubtcnl2ng1s73asfkig-a.oregon-postgres.render.com",
+    host: process.env.DB_HOST || "@dpg-cubtcnl2ng1s73asfkig-a.oregon-postgres.render.com",
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
