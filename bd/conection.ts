7dc97a603e866ff1852fb75e100822a7c296ed84
usamahas17 <ponsiopilato1999@gmail.com>
2025-01-27 22:42:07 -0300

    23 cambio

--- bd/conection.ts
+++ bd/conection.ts
@@ -1,9 +1,17 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-const db = new Sequelize("registro_2025","root","root", {
-    host:'localhost', 
-    dialect: 'mysql',
-    port: 8889, 
+const db = new Sequelize(process.env.DATABASE || "apirest_7i13", process.env.DB_USER || "apirest", process.env.DB_PASSWORD || "oml8ebqCPnAodgLCTgTc5zICw5VvaJd8", {
+    host: process.env.DB_HOST || "dpg-cubtcnl2ng1s73asfkig-a.oregon-postgres.render.com",  
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+          require: true, // Obliga a usar SSL
+          rejectUnauthorized: false, // Acepta certificados autofirmados (opcional)
+        },
+      },
+    port: 5432
 
 });
 
--- dist/bd/conection.js
+++ dist/bd/conection.js
@@ -1,10 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const db = new sequelize_1.Sequelize("registro_2025", "root", "root", {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 8889,
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const db = new sequelize_1.Sequelize(process.env.DATABASE || "apirest_7i13", process.env.DB_USER || "apirest", process.env.DB_PASSWORD || "oml8ebqCPnAodgLCTgTc5zICw5VvaJd8", {
+    host: process.env.DB_HOST || "dpg-cubtcnl2ng1s73asfkig-a.oregon-postgres.render.com",
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // Acepta certificados autofirmados (opcional)
+        },
+    },
+    port: 5432
 });
 exports.default = db;
 //# sourceMappingURL=conection.js.map
